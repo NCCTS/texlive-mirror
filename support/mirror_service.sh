754245cf803e5e2204b40f50a11b620f8e6bbb68
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-24 04:26:40 -0600

    setuser vs sudo

--- support/mirror_service.sh
+++ support/mirror_service.sh
@@ -4,4 +4,7 @@ touch /var/log/mirror_server.log
 chown root:sailor /var/log/mirror_server.log
 chmod 664 /var/log/mirror_server.log
 
-exec sudo -i -u sailor -- bash -i -c 'mirror_server.sh >>/var/log/mirror_server.log 2>&1'
+exec /sbin/setuser sailor bash -c 'cd /home/sailor && \
+                                   source $HOME/.nvm/nvm.sh && \
+                                   exec bin/mirror_server.sh \
+                                   >>/var/log/mirror_server.log 2>&1'
