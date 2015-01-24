f651c02199d5e2befbcf565e38d5c61c3a83b98d
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-24 00:17:16 -0600

    install the mirror-server service

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 export DEBIAN_FRONTEND=noninteractive
 export HOME=/root
 
+# Install phusion/baseimage service to invoke the http mirror-server
+mkdir -p /etc/service/texlive-mirror
+cp /docker-build/support/mirror_service.sh /etc/service/texlive-mirror/run
+
 # Create a local mirror of TeX Live
 /docker-build/support/mirror.sh
 mv /docker-build/support/texlive-mirror /home/sailor
